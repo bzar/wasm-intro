5dcd4f3a99814a23bae257767314a7113d9ad141
Teemu Erkkola <teemu.erkkola@iki.fi>
2017-04-14 13:43:54 +0300

    Animated webgl example

--- webgl/main.c
+++ webgl/main.c
@@ -8,6 +8,8 @@ extern void glEnable(unsigned int);
 extern void glDepthFunc(unsigned int);
 extern void glClear(unsigned int);
 extern int glGetAttribLocation(unsigned int, char*);
+extern int glGetUniformLocation(unsigned int, char*);
+extern void glUniform4fv(int, float, float, float, float);
 extern unsigned int glCreateBuffer();
 extern void glBindBuffer(unsigned int, unsigned int);
 extern void glBufferData(unsigned int, float*, unsigned int, unsigned int);
@@ -40,8 +42,9 @@ int height;
 
 char vertexShader[] = 
 "attribute vec4 a_position;"
+"uniform vec4 u_offset;"
 "void main() {"
-"  gl_Position = a_position;"
+"  gl_Position = a_position + u_offset;"
 "}";
 
 char fragmentShader[] =
@@ -49,6 +52,11 @@ char fragmentShader[] =
 "void main() {"
 " gl_FragColor = vec4(0.3, 0.6, 1.0, 1.0);"
 "}";
+
+unsigned int programId;
+int positionAttributeLocation;
+int offsetUniformLocation;
+unsigned int positionBuffer;
 
 void onInit() {
   width = getCanvasWidth();
@@ -69,15 +77,21 @@ void onInit() {
   unsigned int fsId = compileShader(fs, GL_FRAGMENT_SHADER);
   mfree(fs);
 
-  unsigned int programId = linkShaderProgram(vsId, fsId);
+  programId = linkShaderProgram(vsId, fsId);
 
   char posAttrName[] = "a_position";
   char* positionAttributeName = (char*) malloc(sizeof(posAttrName) - 1);
   memcpy(positionAttributeName, posAttrName, msize(positionAttributeName));
-  int positionAttributeLocation = glGetAttribLocation(programId, positionAttributeName);
+  positionAttributeLocation = glGetAttribLocation(programId, positionAttributeName);
   mfree(positionAttributeName);
 
-  unsigned int positionBuffer = glCreateBuffer();
+  char offsetUnifName[] = "u_offset";
+  char* offsetUniformName = (char*) malloc(sizeof(offsetUnifName) - 1);
+  memcpy(offsetUniformName, offsetUnifName, msize(offsetUniformName));
+  offsetUniformLocation = glGetUniformLocation(programId, offsetUniformName);
+  mfree(offsetUniformName);
+
+  positionBuffer = glCreateBuffer();
   glBindBuffer(GL_ARRAY_BUFFER, positionBuffer);
 
   float positions[] = {0, 0, 0, 0.5, 0.7, 0};
@@ -86,22 +100,27 @@ void onInit() {
   memcpy(positionsPtr, positions, sizeof(positions));
   glBufferData(GL_ARRAY_BUFFER, positionsPtr, num_positions, GL_STATIC_DRAW);
   mfree(positionsPtr);
+}
+
+void onAnimationFrame(int timestamp) {
+  static int previous = 0;
+  static float x = 0;
+
+  int delta = previous ? timestamp - previous : 0;
+  x += delta / 1000.0;
+  if(x > 1) x = -2;
+
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
   glUseProgram(programId);
   glEnableVertexAttribArray(positionAttributeLocation);
   glBindBuffer(GL_ARRAY_BUFFER, positionBuffer);
   glVertexAttribPointer(positionAttributeLocation, 2, GL_FLOAT, 0, 0, 0);
+  glUniform4fv(offsetUniformLocation, x, 0.0f, 0.0f, 0.0f);
   glDrawArrays(GL_TRIANGLES, 0, 3);
-}
-
-void onAnimationFrame(int timestamp) {
-  //static int previous = 0;
-
-  //int delta = previous ? timestamp - previous : 0;
-
   // logic
 
-  //previous = timestamp;
+  previous = timestamp;
 }
 
 // Malloc/mfree/msize
