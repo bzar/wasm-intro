7486028aaf1c5956fe957b5948b55fff5533b16d
Teemu Erkkola <teemu.erkkola@iki.fi>
2017-04-17 14:01:34 +0300

    Convert webgl example to use no dynamic allocation

--- webgl/main.c
+++ webgl/main.c
@@ -1,14 +1,12 @@
-extern int getCanvasWidth();
-extern int getCanvasHeight();
-extern unsigned int compileShader(char* source, unsigned int type);
+extern unsigned int compileShader(char const* source, unsigned int len, unsigned int type);
 extern unsigned int linkShaderProgram(unsigned int vertexShaderId, unsigned int fragmentShaderId);
 
 extern void glClearColor(float, float, float, float);
 extern void glEnable(unsigned int);
 extern void glDepthFunc(unsigned int);
 extern void glClear(unsigned int);
-extern int glGetAttribLocation(unsigned int, char*);
-extern int glGetUniformLocation(unsigned int, char*);
+extern int glGetAttribLocation(unsigned int, char const*, unsigned int);
+extern int glGetUniformLocation(unsigned int, char const*, unsigned int);
 extern void glUniform4fv(int, float, float, float, float);
 extern unsigned int glCreateBuffer();
 extern void glBindBuffer(unsigned int, unsigned int);
@@ -30,16 +28,6 @@ unsigned int GL_LEQUAL = 515;
 unsigned int GL_COLOR_BUFFER_BIT = 16384;
 unsigned int GL_DEPTH_BUFFER_BIT = 256;
 
-
-
-void* malloc(unsigned int);
-void mfree(void*);
-unsigned int msize(void*);
-void* memcpy(void*, void const*, unsigned int);
-
-int width;
-int height;
-
 char vertexShader[] = 
 "attribute vec4 a_position;"
 "uniform vec4 u_offset;"
@@ -53,53 +41,30 @@ char fragmentShader[] =
 " gl_FragColor = vec4(0.3, 0.6, 1.0, 1.0);"
 "}";
 
+float positions[] = {0, 0, 0, 0.5, 0.7, 0};
+
 unsigned int programId;
 int positionAttributeLocation;
 int offsetUniformLocation;
 unsigned int positionBuffer;
 
 void onInit() {
-  width = getCanvasWidth();
-  height = getCanvasHeight();
-
   glClearColor(0.1, 0.1, 0.1, 1.0);
   glEnable(GL_DEPTH_TEST);
   glDepthFunc(GL_LEQUAL);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  char* vs = (char*) malloc(sizeof(vertexShader) - 1);
-  memcpy(vs, vertexShader, msize(vs));
-  unsigned int vsId = compileShader(vs, GL_VERTEX_SHADER);
-  mfree(vs);
-
-  char* fs = (char*) malloc(sizeof(fragmentShader) - 1);
-  memcpy(fs, fragmentShader, msize(fs));
-  unsigned int fsId = compileShader(fs, GL_FRAGMENT_SHADER);
-  mfree(fs);
+  unsigned int vsId = compileShader(vertexShader, sizeof(vertexShader) - 1, GL_VERTEX_SHADER);
+  unsigned int fsId = compileShader(fragmentShader, sizeof(fragmentShader) - 1, GL_FRAGMENT_SHADER);
 
   programId = linkShaderProgram(vsId, fsId);
 
-  char posAttrName[] = "a_position";
-  char* positionAttributeName = (char*) malloc(sizeof(posAttrName) - 1);
-  memcpy(positionAttributeName, posAttrName, msize(positionAttributeName));
-  positionAttributeLocation = glGetAttribLocation(programId, positionAttributeName);
-  mfree(positionAttributeName);
-
-  char offsetUnifName[] = "u_offset";
-  char* offsetUniformName = (char*) malloc(sizeof(offsetUnifName) - 1);
-  memcpy(offsetUniformName, offsetUnifName, msize(offsetUniformName));
-  offsetUniformLocation = glGetUniformLocation(programId, offsetUniformName);
-  mfree(offsetUniformName);
+  positionAttributeLocation = glGetAttribLocation(programId, "a_position", sizeof("a_position") - 1);
+  offsetUniformLocation = glGetUniformLocation(programId, "u_offset", sizeof("u_offset") - 1);
 
   positionBuffer = glCreateBuffer();
   glBindBuffer(GL_ARRAY_BUFFER, positionBuffer);
-
-  float positions[] = {0, 0, 0, 0.5, 0.7, 0};
-  unsigned int num_positions = sizeof(positions)/sizeof(float);
-  float* positionsPtr = (float*) malloc(sizeof(positions));
-  memcpy(positionsPtr, positions, sizeof(positions));
-  glBufferData(GL_ARRAY_BUFFER, positionsPtr, num_positions, GL_STATIC_DRAW);
-  mfree(positionsPtr);
+  glBufferData(GL_ARRAY_BUFFER, positions, 6, GL_STATIC_DRAW);
 }
 
 void onAnimationFrame(int timestamp) {
@@ -118,60 +83,5 @@ void onAnimationFrame(int timestamp) {
   glVertexAttribPointer(positionAttributeLocation, 2, GL_FLOAT, 0, 0, 0);
   glUniform4fv(offsetUniformLocation, x, 0.0f, 0.0f, 0.0f);
   glDrawArrays(GL_TRIANGLES, 0, 3);
-  // logic
-
   previous = timestamp;
-}
-
-// Malloc/mfree/msize
-
-typedef struct Block {
-  unsigned int size;
-  unsigned int usize;
-  struct Block* next;
-  struct Block* prev;
-} Block;
-
-Block* usedBlocks = 0;
-Block* freeBlocks = 0;
-Block* nextBlock = (Block*) 1024;
-
-void* malloc(unsigned int size) {
-  Block* b = freeBlocks;
-  while (b && b->size < size) b = b->next;
-
-  if(!b) {
-    b = nextBlock;
-    b->size = size;
-    nextBlock += size + sizeof(Block); // Header + data
-  }
-
-  b->usize = size;
-  b->next = usedBlocks;
-  if(b->next) b->next->prev = b;
-  b->prev = 0;
-  usedBlocks = b;
-
-  return (void*) b + sizeof(Block);
-}
-
-void mfree(void* data) {
-  Block* b = data - sizeof(Block);
-  if(b->prev) b->prev->next = b->next;
-  b->next = freeBlocks;
-  if(b->next) b->next->prev = b;
-  b->prev = 0;
-  freeBlocks = b;
-}
-
-unsigned int msize(void* data) {
-  Block* b = data - sizeof(Block);
-  return b->usize;
-}
-
-void* memcpy(void* dst, void const* src, unsigned int bytes) {
-  for(unsigned int i = 0; i < bytes; ++i) {
-    ((char*)dst)[i] = ((char*)src)[i];
-  }
-  return dst;
 }
